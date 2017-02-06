4e8284b06dc2dea5b2fcfc6ee7a17ea243765452
Ranjeet482 <rajababusingh@a2md17257m.local>
2017-02-06 12:42:28 +0530

    Initial Commit

---
+++ RemoteServer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemoteServer
+//
+//  Created by raja babu singh on 06/02/17.
+//  Copyright © 2017 raja babu singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
